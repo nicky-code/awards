2c57760133bcd0a827ede1e39054f7fbb5d74551
nicky-code <aline.nicole7@gmail.com>
2019-10-25 01:32:16 +0200

    installed the bootstrap4 on my application.

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+from django.shortcuts import render
+from .models import Projects,Profile
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
