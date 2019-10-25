4719f36fa5449b1af9c1038bd408a89814d72866
nicky-code <aline.nicole7@gmail.com>
2019-10-25 13:33:24 +0200

    added the authentication.

--- awards/views.py
+++ awards/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 from django.shortcuts import render
 from .models import Projects,Profile
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'welcome.html')
