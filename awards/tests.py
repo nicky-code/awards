8ce1987a92fe92435428d9f5e405e9c8567b29f0
nicky-code <aline.nicole7@gmail.com>
2019-10-27 11:04:53 +0200

    testing the instances of projects.

--- awards/tests.py
+++ awards/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Projects
+from .models import Projects,Profile
 from django.contrib.auth.models import User
 
 # Create your tests here.
@@ -8,11 +8,23 @@ class ProjectsTestClass(TestCase):
     a class to test the projects instances and its methods
     '''
 
-    # # Set up method
-    # def setUp(self):
-    #     self.user = User.objects.create(id=1,username='nicky')
-    #     self.projects= Projects(title='Sodo-Ni Pizzeria',image='',project_description='Our favorites Pizzas', link='https://nicky-code.github.io/PizzaLover/',user=self.user)
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create(id=1,username='nicky')
+        self.projects= Projects(title='Sodo-Ni Pizzeria',image='',project_description='Our favorites Pizzas', link='https://nicky-code.github.io/PizzaLover/',design='7',usability='8',content='7',user=self.user)
     
-    # #Testing Instance
-    # def test_instance(self):
-    #     self.assertTrue(isinstance(self.projects,Projects))
+    #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.projects,Projects))
+        
+    
+     #Testing the save method
+    '''
+    function to check the save method of Projects
+    '''
+    
+    def test_save_method(self):
+        self.title.save_projects()
+        title=Projects.objects.all()
+        self.assertTrue(len(title)>=1)
+           
