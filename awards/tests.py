9f6435c473206162de8a8c243e458f42527b9e15
nicky-code <aline.nicole7@gmail.com>
2019-10-31 10:50:39 +0200

    test for deleting a project.

--- awards/tests.py
+++ awards/tests.py
@@ -24,24 +24,37 @@ class ProjectsTestClass(TestCase):
     '''
     
     def test_save_method(self):
-        self.image.save_projects()
-       images =Projects.objects.all()
-       self.assertTrue(len(images)>0)
-
-
-class ProfileTestClass(TestCase):
-    '''
-    a class to test the profile instances and its methods
-    '''
-
-    # Set up method
-    def setUp(self):
-        self.user = User.objects.create(id=1,username='nicky')
-        self.profile= Profile(profile_picture='',bio='alinenicole',posted_projects='Our favorites Pizzas', link='https://nicky-code.github.io/PizzaLover/',design='7',usability='8',content='7',user=self.user)
+        
+        self.projects.save_projects()
+        projects =Projects.objects.all()
+        self.assertTrue(len(projects)>0)
+       
     
-    #Testing Instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.projects,Projects))
+    #Testing the delete method
+    def test_delete_method(self):
+        '''
+        function to check the delete method of projects
+        '''
+        self.projects.save_projects()
+        projected= Projects.objects.filter(title='Sodo-Ni Pizzeria').first()
+        delete= Projects.objects.filter(id=projected.id).delete()
+        project=Projects.objects.all()
+        self.assertFalse(len(project)==1)
+
+
+# class ProfileTestClass(TestCase):
+#     '''
+#     a class to test the profile instances and its methods
+#     '''
+
+#     # Set up method
+#     def setUp(self):
+#         self.user = User.objects.create(id=1,username='nicky')
+#         self.profile= Profile(profile_picture='',bio='alinenicole',posted_projects='Our favorites Pizzas', link='https://nicky-code.github.io/PizzaLover/',design='7',usability='8',content='7',user=self.user)
+    
+#     #Testing Instance
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.projects,Projects))
         
     
     # #Testing the save method
